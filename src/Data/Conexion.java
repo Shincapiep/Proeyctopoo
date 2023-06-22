60b87a7df35e9849bb8176a7f43faa4a024febb0
JRubianov <136925455+JRubianov@users.noreply.github.com>
2023-06-22 11:10:53 -0500

    format

--- src/Data/Conexion.java
+++ src/Data/Conexion.java
@@ -3,28 +3,25 @@ package Data;
 import java.sql.*;
 
 public class Conexion {
-    
-   // Connection conectar=null;
-        
-       /* static String usuario = "usercine";
+
+    // Connection conectar=null;
+    /* static String usuario = "usercine";
         static String contrasena="1234";
         static String database="Cine";
         static String ip = "localhost";
         static String puerto = "1433";
         static String cadena ="jdbc:sqlserver://"+ip+":"+puerto;*/
-
     public static Connection ObtenerConeccion() {
-        
-       // Connection conectar=null;
-        
-       String url = "jdbc:sqlserver://localhost:1433;"
+
+        // Connection conectar=null;
+        String url = "jdbc:sqlserver://localhost:1433;"
                 + "user=usercine;"
                 + "password=cine123;"
                 + "database=Cine;"
-                + "encrypt=false;"; 
+                + "encrypt=false;";
         try {
             //Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-           //cadena ="jdbc:sqlserver://localhost:"+puerto+";"+"Cine"+database;
+            //cadena ="jdbc:sqlserver://localhost:"+puerto+";"+"Cine"+database;
             //conectar=DriverManager.getConnection(cadena,usuario,contrasena);
             Connection con = DriverManager.getConnection(url);
             return con;
@@ -32,7 +29,7 @@ public class Conexion {
             System.out.println(ex.toString());
             return null;
         }
-            //return conectar;
+        //return conectar;
     }
 
 }
