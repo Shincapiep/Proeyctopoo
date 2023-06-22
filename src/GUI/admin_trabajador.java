6678a613d49dd4e41b08794e5d4f6f12d2d859ad
LENOVO <LENOVO@192.168.10.15>
2023-06-22 17:57:31 -0500

    Admin listo excepcion reset

--- src/GUI/admin_trabajador.java
+++ src/GUI/admin_trabajador.java
@@ -10,7 +10,7 @@ import Data.*;
  * @author LENOVO
  */
 public class admin_trabajador extends javax.swing.JFrame {
-
+   Trabajador Trabaja1 = new Trabajador("Juan gonzales","98635169","3504676261");
     /**
      * Creates new form admin_trabajador
      */
@@ -102,20 +102,20 @@ public class admin_trabajador extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-       Trabajador Trabaja1 = new Trabajador("Juan gonzales","98635169","3504676261");
+
        Trabaja1.ingresar_info_Trabajador();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-       //Trabaja1.
+       Trabaja1.Eliminar_trabajador();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        Trabaja1.buscar_Trabajador();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
+        Trabaja1.leer_Trabajador();
     }//GEN-LAST:event_jButton4ActionPerformed
 
 
